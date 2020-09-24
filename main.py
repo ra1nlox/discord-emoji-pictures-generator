a3684456319ab132323410d66bbaacd1cecf03a1
ra1nlox <a.borzoi@yandex.ru>
2020-09-24 10:01:46 +0300

    added function 'generate_square7(7x7)'

--- main.py
+++ main.py
@@ -22,19 +22,25 @@ def cli():
 @cli.command()
 @click.option('-n', '--number', nargs=1, type=int)
 def set_rows(number):
-    config.set('settings', 'rows', str(number))
-    with open("config.ini", 'w') as config_file:
-        config.write(config_file)
+    if number <= 8:
+        config.set('settings', 'rows', str(number))
+        with open("config.ini", 'w') as config_file:
+            config.write(config_file)
+    else:
+        print('Error: "number" is bigger than 8')
 
 @cli.command()
 @click.option('-n', '--number', nargs=1, type=int)
 def set_length(number):
-    config.set('settings', 'length', str(number))
-    with open("config.ini", 'w') as config_file:
-        config.write(config_file)
+    if number <= 8:
+        config.set('settings', 'length', str(number))
+        with open("config.ini", 'w') as config_file:
+            config.write(config_file)
+    else:
+        print('Error: "number" is bigger than 8')
 
 @cli.command()
-@click.option('-n', '--number', nargs=1, type=int, help='Number of ')
+@click.option('-n', '--number', nargs=1, type=int, help='Number of emoji')
 @click.argument('name')
 def set_emoji(number, name):
     name0 = f':{name}:'
@@ -50,6 +56,31 @@ def set_emoji(number, name):
         config.set('emojis', 'emoji3', str(name0))
         with open("config.ini", 'w') as config_file:
             config.write(config_file)
+
+@cli.command()
+def generate_square7():
+    if int(max_length) <= 7 and int(max_rows) <= 7:
+        row0 = emoji1*7
+        row1 = emoji1*2 + emoji2*3 +emoji1*2
+        row2 = emoji1 + emoji2 + emoji1*3 + emoji2 + emoji1
+        row3 = emoji1 + emoji2 + emoji1*3 + emoji2 + emoji1
+        row4 = emoji1 + emoji2 + emoji1*3 + emoji2 + emoji1
+        row5 = emoji1*2 + emoji2*3 +emoji1*2
+        row6 = emoji1*7
+
+        config.set('output', 'output0', row0)
+        config.set('output', 'output1', row1)
+        config.set('output', 'output2', row2)
+        config.set('output', 'output3', row3)
+        config.set('output', 'output4', row4)
+        config.set('output', 'output5', row5)
+        config.set('output', 'output6', row6)
+        with open("config.ini", 'w') as config_file:
+            config.write(config_file)
+
+    else:
+        print("Error: 'length' is too big")
+        print(max_length)
 
 if __name__ == '__main__':
     cli()
